21a9aa70b7466ac6ed718a90cd9885cfe473dd46
xpp012 <120717353+Eryoneoffical@users.noreply.github.com>
2025-06-26 14:43:56 +0800

    2

--- config/plr.sh
+++ config/plr.sh
@@ -5,22 +5,23 @@ rm ${SD_PATH}/plr.gcode
 #SD_PATH=~octoprint/.octoprint/uploads
 cat "${2}" > /tmp/plrtmpA.$$
 cat /tmp/plrtmpA.$$ | sed -n '/HEADER_BLOCK_START/,/THUMBNAIL_BLOCK_END/{p}' > ${SD_PATH}/plr.gcode
+echo '' >> ${SD_PATH}/plr.gcode
 sed -i 's/Z\./Z0\./g' /tmp/plrtmpA.$$
 #sed -i 's/z./z0./g' /tmp/plrtmpA.$$
 #cat /tmp/plrtmpA.$$ | sed -e '1,/Z'${1}'/ d' | sed -ne '/ Z/,$ p' | grep -m 1 ' Z' | sed -ne 's/.* Z\([^ ]*\)/SET_KINEMATIC_POSITION Z=\1/p' > ${SD_PATH}/plr.gcode
 
 #echo 'START_TEMPS' >> ${SD_PATH}/plr.gcode
-cat /tmp/plrtmpA.$$ | sed '/ Z'${1}'/q' | sed -ne '/\(M104\|M140\|M109\|M190\|M106\)/p' >> ${SD_PATH}/plr.gcode
+cat /tmp/plrtmpA.$$ | sed '/G1 Z'${1}'/q' | sed -ne '/\(M104\|M140\|M109\|M190\|M106\)/p' >> ${SD_PATH}/plr.gcode
 cat /tmp/plrtmpA.$$ | sed -ne '/;End of Gcode/,$ p' | tr '\n' ' ' | sed -ne 's/ ;[^ ]* //gp' | sed -ne 's/\\\\n/;/gp' | tr ';' '\n' | grep material_bed_temperature | sed -ne 's/.* = /M140 S/p' | head -1 >> ${SD_PATH}/plr.gcode
 cat /tmp/plrtmpA.$$ | sed -ne '/;End of Gcode/,$ p' | tr '\n' ' ' | sed -ne 's/ ;[^ ]* //gp' | sed -ne 's/\\\\n/;/gp' | tr ';' '\n' | grep material_print_temperature | sed -ne 's/.* = /M104 S/p' | head -1 >> ${SD_PATH}/plr.gcode
 cat /tmp/plrtmpA.$$ | sed -ne '/;End of Gcode/,$ p' | tr '\n' ' ' | sed -ne 's/ ;[^ ]* //gp' | sed -ne 's/\\\\n/;/gp' | tr ';' '\n' | grep material_bed_temperature | sed -ne 's/.* = /M190 S/p' | head -1 >> ${SD_PATH}/plr.gcode
 cat /tmp/plrtmpA.$$ | sed -ne '/;End of Gcode/,$ p' | tr '\n' ' ' | sed -ne 's/ ;[^ ]* //gp' | sed -ne 's/\\\\n/;/gp' | tr ';' '\n' | grep material_print_temperature | sed -ne 's/.* = /M109 S/p' | head -1 >> ${SD_PATH}/plr.gcode
 # cat /tmp/plrtmpA.$$ | sed -e '1,/ Z'${1}'[^0-9]*$/ d' | sed -e '/ Z/q' | tac | grep -m 1 ' E' | sed -ne 's/.* E\([^ ]*\)/G92 E\1/p' >> ${SD_PATH}/plr.gcode
 #tac /tmp/plrtmpA.$$ | sed -e '/ Z'${1}'[^0-9]*$/q' | tac | tail -n+2 | sed -e '/ Z[0-9]/ q' | tac | sed -e '/ E[0-9]/ q' | sed -ne 's/.* E\([^ ]*\)/G92 E\1/p' >> ${SD_PATH}/plr.gcode
-BG_EX=`tac /tmp/plrtmpA.$$ | sed -e '/ Z'${1}'[^0-9]*$/q' | tac | tail -n+2 | sed -e '/ Z[0-9]/ q' | tac | sed -e '/ E[0-9]/ q' | sed -ne 's/.* E\([^ ]*\)/G92 E\1/p'`
+BG_EX=`tac /tmp/plrtmpA.$$ | sed -e '/G1 Z'${1}'[^0-9]*$/q' | tac | tail -n+2 | sed -e '/ Z[0-9]/ q' | tac | sed -e '/ E[0-9]/ q' | sed -ne 's/.* E\([^ ]*\)/G92 E\1/p'`
 # If we failed to match an extrusion command (allowing us to correctly set the E axis) prior to the matched layer height, then simply set the E axis to the first E value present in the resemued gcode.  This avoids extruding a huge blod on resume, and/or max extrusion errors.
 if [ "${BG_EX}" = "" ]; then
- BG_EX=`tac /tmp/plrtmpA.$$ | sed -e '/ Z'${1}'[^0-9]*$/q' | tac | tail -n+2 | sed -ne '/ Z/,$ p' | sed -e '/ E[0-9]/ q' | sed -ne 's/.* E\([^ ]*\)/G92 E\1/p'`
+ BG_EX=`tac /tmp/plrtmpA.$$ | sed -e '/G1 Z'${1}'[^0-9]*$/q' | tac | tail -n+2 | sed -ne '/ Z/,$ p' | sed -e '/ E[0-9]/ q' | sed -ne 's/.* E\([^ ]*\)/G92 E\1/p'`
 fi
 echo 'G92 E0' >> ${SD_PATH}/plr.gcode
 echo 'M83' >> ${SD_PATH}/plr.gcode
@@ -42,7 +43,7 @@ echo 'G1 E2' >> ${SD_PATH}/plr.gcode
 echo 'G1 X350 Y350 F6000' >> ${SD_PATH}/plr.gcode
 echo 'G92 E0' >> ${SD_PATH}/plr.gcode
 echo 'SET_KINEMATIC_POSITION Z='$1 >> ${SD_PATH}/plr.gcode
-cat /tmp/plrtmpA.$$ | sed -e '1,/Z'${1}'/ d' | sed -ne '/ Z/,$ p' >> ${SD_PATH}/plr.gcode
+cat /tmp/plrtmpA.$$ | sed -e '1,/G1 Z'${1}'/d' | sed -ne '/ Z/,$ p' >> ${SD_PATH}/plr.gcode
 #tac /tmp/plrtmpA.$$ | sed -e '/ Z'${1}'[^0-9]*$/q' | tac | tail -n+2 | sed -ne '/ Z/,$ p' >> ${SD_PATH}/plr.gcode
 if [ $(stat -c %s ${SD_PATH}/plr.gcode) -gt 1024 ]; then
   echo ">lk"
